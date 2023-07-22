459bbb6527d15a888b6aeef5617b19849be245a4
Tahawahid <muhammadtahawahid1@gmail.com>
2023-07-22 18:25:48 +0500

    build Analytics compenent

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Laptop from "../assests/3156627.jpg";
+
+const Analytics = () => {
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img className="w-[500px] mx-auto my-4" src={Laptop} alt="analytics" />
+        <div className="flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Manage Data Analytics Centrally
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus id
+            vitae hic praesentium quo rem expedita nulla quibusdam deserunt
+            corrupti quas maiores quod amet dolore quis incidunt deleniti, cum
+            delectus.
+          </p>
+          <button className="bg-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;

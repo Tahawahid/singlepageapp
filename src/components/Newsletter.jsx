91117097818dbfa67e60ebcad2be0273d2cd9971
Tahawahid <muhammadtahawahid1@gmail.com>
2023-07-24 19:34:37 +0500

    fix some alignments

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,7 +4,7 @@ const Newsletter = () => {
   return (
     <div className="w-full text-white py-16 px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
-        <div className="lg:col-span-2">
+        <div className="lg:col-span-2 my-4">
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
             Want tips & tricks to optimize your flow?
           </h1>
@@ -24,6 +24,10 @@ const Newsletter = () => {
               Notify Me
             </button>
           </div>
+          <p>
+            We care about the protection of your data. Read our{" "}
+            <span className="text-[#00df9a]">Privacy Policy. </span>
+          </p>
         </div>
       </div>
     </div>
